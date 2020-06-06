5efda38036d12131b8c9b11756191928429d328d
Matt Jarrett <cujarrett@gmail.com>
2020-06-06 01:29:29 -0500

    :tada: Ability to serve my resume in JSON format over REST API

---
+++ README.md
@@ -0,0 +1,9 @@
+## What
+Open source REST API for my [JSON-based standard format resume](https://jsonresume.org/).
+
+## Why
+To build something simple with Go. To expose my resume as an API as [JSON-based standard for resumes](https://jsonresume.org/).
+
+### Why Go?
+- It's compiled. So you get small binaries.
+- It's fast. (slower than c/c++ or rust) but faster than most other web programming languages.
---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetResume(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handleRouting)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	data, _ := ioutil.ReadFile("./expected.json")
+	expected := string(data)
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
